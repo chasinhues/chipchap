47182e8f5fbe5fb45940d3229da2df3373f0b12a
Jason Hughes <jasonhughes1@outlook.com>
2022-01-01 16:17:44 +1000

    update build config

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,21 @@ import { babel } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
 
 const config = {
   input: "src/index.tsx",
   output: {
-    file: "bundle.js",
-    format: "esm",
+    file: pkg.module,
+    format: "es",
   },
   plugins: [
     nodeResolve(),
     typescript(),
     commonjs(),
     babel({ babelHelpers: "bundled" }),
+    terser(),
   ],
 };
 
